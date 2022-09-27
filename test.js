80ca05d095d5a90853658e95f640605c0ac08f67
William <williamliu317@yahoo.com>
2022-09-26 20:42:15 -0400

    week 1 day 1

--- test.js
+++ test.js
@@ -1,1 +1,39 @@
-console.log("hi");
+console.log(2)
+console.log(2+2)
+console.log(2+5)
+console.log(10-3)
+console.log(10/3)
+console.log(10%3)
+console.log(9%2)
+console.log(10%2)
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
+console.log(true || true)
+console.log(!true)
+console.log(!false)
+console.log(!!!false)
+console.log(true && true)
+let str = 'hello'
+console.log(str[1])
+let str2 = 'hello'
+console.log("hello".indexOf('h'))
+console.log("race".concat("car"));
+console.log(4+"hi")
+console.log('hello'.toUpperCase());
+console.log('HELLO'.toLowerCase());
+let name = 'Jonathan';
+console.log(name)
+console.log(name==="Jonathan")
+name="Kevin"
+console.log(name)
+name = 'Victor'
+console.log(name)
+let test
+console.log(test)
+if (20===20){
+    console.log('they are not equal');
+}else{
+    console.log('they are not equal');
+}
